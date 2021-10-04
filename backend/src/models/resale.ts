165aea7d83fc5f41d33e030aab3b985d9f3c6d80
shadowezz <wenjie.cao@hotmail.com>
2021-10-04 21:50:44 +0800

    Create resaleFlat model.

---
+++ backend/src/models/resale.ts
@@ -0,0 +1,44 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { FlatType, Town } from '../utils/model';
+
+@Entity()
+export default class ResaleFlat {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('int')
+  transactionDate: number;
+
+  @Column('enum', { enum: Town })
+  location: Town;
+
+  @Column('enum', { enum: FlatType })
+  flatType: FlatType;
+
+  @Column('text')
+  flatModel: string;
+
+  @Column('int')
+  block: number;
+
+  @Column('text')
+  streetName: string;
+
+  @Column('int')
+  floorArea: number; // sqm
+
+  @Column('int')
+  minStorey: number;
+
+  @Column('int')
+  maxStorey: number;
+
+  @Column('int')
+  leaseCommenceYear; // year
+
+  @Column('int')
+  remainingLease: number; // months
+
+  @Column('int')
+  resalePrice: number;
+}
