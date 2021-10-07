95e42405265c84f4904c8a687776da8cd0aada80
chrystalquek <54176678+chrystalquek@users.noreply.github.com>
2021-10-07 20:57:16 +0800

    Clean up code

--- backend/src/controllers/heatMap.ts
+++ backend/src/controllers/heatMap.ts
@@ -2,25 +2,26 @@ import Koa from 'koa';
 import resaleService from '../services/resale';
 import { Town } from '../utils/model';
 
+const parseYears = (queryYears: string | string[] | undefined) =>
+  Array.isArray(queryYears)
+    ? queryYears.map((year) => Number(year))
+    : [Number(queryYears)];
+
 const getResalesByIsland = async (ctx: Koa.Context): Promise<void> => {
   const resales = await resaleService.getResalesByIsland({
-    years: Array.isArray(ctx.query.years)
-      ? ctx.query.years.map((year) => Number(year))
-      : [Number(ctx.query.years)],
+    years: parseYears(ctx.query.years),
   });
 
   ctx.body = { data: resales };
 };
 
 const getResalesByTown = async (ctx: Koa.Context): Promise<void> => {
-  const blockPrices = await resaleService.getResalesByTown({
-    years: Array.isArray(ctx.query.years)
-      ? ctx.query.years.map((year) => Number(year))
-      : [Number(ctx.query.years)],
+  const resales = await resaleService.getResalesByTown({
+    years: parseYears(ctx.query.years),
     town: ctx.query.town as Town,
   });
 
-  ctx.body = { data: blockPrices };
+  ctx.body = { data: resales };
 };
 
 export default {
--- backend/src/services/resale.ts
+++ backend/src/services/resale.ts
@@ -1,9 +1,4 @@
-import {
-  createQueryBuilder,
-  FindConditions,
-  getRepository,
-  Raw,
-} from 'typeorm';
+import { getRepository } from 'typeorm';
 import Resale from '../models/resale';
 import { Town } from '../utils/model';
 
@@ -18,39 +13,21 @@ export type QueryResaleByIsland = {
 
 const getResalesByIsland = async (
   queries: QueryResaleByIsland
-): Promise<Array<{ town: Town; resalePrice: number }>> => {
-  const hii = 'rqwrw';
-  return (
-    getRepository(Resale)
-      .createQueryBuilder('resale')
-      // .select(['resale.town'])
-      .select('resale.town', 'town')
-      // .select('resale.id', 'resale.id')
-      // .addSelect('resale.transactionDate', 'transaction_date')
-      // .addSelect('resale.town', 'town')
-      .addSelect('AVG(resale.resalePrice)', 'resalePrice')
-      .where('date_part(\'year\', "transactionDate") IN (:...years)', {
-        years: queries.years,
-      })
-      .groupBy('town')
-      .getRawMany()
-    // .getMany()
-  );
+): Promise<Array<{ town: Town; resalePrice: number }>> =>
+  getRepository(Resale)
+    .createQueryBuilder('resale')
+    .select('resale.town', 'town')
+    .addSelect('AVG(resale.resalePrice)', 'resalePrice')
+    .where('date_part(\'year\', "transactionDate") IN (:...years)', {
+      years: queries.years,
+    })
+    .groupBy('town')
+    .getRawMany();
 
-  // .map((value: Array<Resale>, key: Town) => ({
-  //   town: key,
-  //   resalePrice: _.meanBy(value, (resale) => resale.resalePrice),
-  // }));
-};
-
-// { address: string; resalePrice: number }
 const getResalesByTown = async (
   queries: QueryResaleByTown
-): Promise<Array<{ town: Town; resalePrice: number }>> => {
-  const hii = 'rqwrw';
-  console.log(hii);
-  console.log(queries);
-  return getRepository(Resale)
+): Promise<Array<{ address: string; resalePrice: number }>> =>
+  getRepository(Resale)
     .createQueryBuilder('resale')
     .select("CONCAT (resale.block, ' ', resale.streetName)", 'address')
     .addSelect('AVG(resale.resalePrice)', 'resalePrice')
@@ -60,41 +37,8 @@ const getResalesByTown = async (
     .andWhere({ town: queries.town })
     .groupBy('address')
     .getRawMany();
-  // .getRawMany()
-};
-
-const getResales = async (
-  queries: QueryResaleByTown
-): Promise<Array<Resale>> => {
-  const conditions: FindConditions<Resale> = {};
-  if (queries.years) {
-    // check that each ResaleFlat's transactionDate's year is one of years specified
-    const years =
-      typeof queries.years === 'string' || typeof queries.years === 'number'
-        ? [Number(queries.years)]
-        : queries.years.map((year) => Number(year));
-
-    conditions.transactionDate = Raw(
-      (transactionDate) =>
-        `date_part('year', ${transactionDate}) IN (:...years)`,
-      {
-        years,
-      }
-    );
-  }
-
-  if (queries.town) {
-    conditions.town = queries.town;
-  }
-
-  return getRepository(Resale).find({
-    where: conditions,
-    cache: true,
-  });
-};
 
 export default {
   getResalesByIsland,
   getResalesByTown,
-  getResales,
 };
