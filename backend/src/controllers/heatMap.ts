36e605b273e11ce3c592524d8dd119c70abbbc88
chrystalquek <54176678+chrystalquek@users.noreply.github.com>
2021-10-19 22:32:27 +0800

    Add filters to SQL queries

--- backend/src/controllers/heatMap.ts
+++ backend/src/controllers/heatMap.ts
@@ -1,29 +1,14 @@
 import Koa from 'koa';
 import resaleService from '../services/heatMap';
-import { Town } from '../utils/model';
-
-const parseYears = (queryYears: string | string[] | undefined) => {
-  if (!queryYears) {
-    return undefined;
-  }
-  return Array.isArray(queryYears)
-    ? queryYears.map((year) => Number(year))
-    : [Number(queryYears)];
-};
 
 const getResalesByIsland = async (ctx: Koa.Context): Promise<void> => {
-  const resales = await resaleService.getResalesByIsland({
-    years: parseYears(ctx.query.years),
-  });
+  const resales = await resaleService.getResalesByIsland(ctx.query);
 
   ctx.body = { data: resales };
 };
 
 const getResalesByTown = async (ctx: Koa.Context): Promise<void> => {
-  const resales = await resaleService.getResalesByTown({
-    years: parseYears(ctx.query.years),
-    town: ctx.query.town as Town,
-  });
+  const resales = await resaleService.getResalesByTown(ctx.query);
 
   ctx.body = { data: resales };
 };
--- backend/src/services/heatMap.ts
+++ backend/src/services/heatMap.ts
@@ -1,14 +1,101 @@
-import { getRepository } from 'typeorm';
+import { getRepository, SelectQueryBuilder } from 'typeorm';
+import hdbLease from '../utils/constants';
 import Resale from '../models/resale';
 import { FlatType, Town } from '../utils/model';
 
 export type QueryResaleByTown = {
   years?: number[];
   town?: Town;
+  flatTypes?: FlatType[] | FlatType;
+  minStorey?: number;
+  maxStorey?: number;
+  minFloorArea?: number;
+  maxFloorArea?: number;
+  minLeasePeriod?: number;
+  maxLeasePeriod?: number;
 };
 
 export type QueryResaleByIsland = {
   years?: number[];
+  flatTypes?: FlatType[] | FlatType;
+  minStorey?: number;
+  maxStorey?: number;
+  minFloorArea?: number;
+  maxFloorArea?: number;
+  minLeasePeriod?: number;
+  maxLeasePeriod?: number;
+};
+
+const addWhereClauses = (
+  queries: QueryResaleByTown | QueryResaleByIsland,
+  queryBuilder: SelectQueryBuilder<Resale>
+) => {
+  if (queries.years) {
+    queryBuilder.where(
+      'date_part(\'year\', "transactionDate") IN (:...years)',
+      {
+        years: queries.years instanceof Array ? queries.years : [queries.years],
+      }
+    );
+  }
+
+  if (queries.flatTypes) {
+    queryBuilder.andWhere('resale.flatType IN (:...flatTypes)', {
+      flatTypes:
+        queries.flatTypes instanceof Array
+          ? queries.flatTypes
+          : [queries.flatTypes],
+    });
+  }
+
+  // as long as there is an overlap in stories
+  if (queries.minStorey) {
+    queryBuilder.andWhere('resale.maxStorey >= :minStorey', {
+      minStorey: queries.minStorey,
+    });
+  }
+
+  if (queries.maxStorey) {
+    queryBuilder.andWhere('resale.minStorey <= :maxStorey', {
+      maxStorey: queries.maxStorey,
+    });
+  }
+
+  if (queries.minFloorArea) {
+    queryBuilder.andWhere('resale.floorArea >= :minFloorArea', {
+      minFloorArea: queries.minFloorArea,
+    });
+  }
+
+  if (queries.maxFloorArea) {
+    queryBuilder.andWhere('resale.floorArea <= :maxFloorArea', {
+      maxFloorArea: queries.maxFloorArea,
+    });
+  }
+
+  if (queries.minLeasePeriod) {
+    const currentDate = new Date();
+    const targetLeaseCommence = new Date(
+      currentDate.getFullYear() - hdbLease + Number(queries.minLeasePeriod),
+      currentDate.getMonth()
+    );
+
+    queryBuilder.andWhere('resale.leaseCommenceYear >= :minDate', {
+      minDate: targetLeaseCommence,
+    });
+  }
+
+  if (queries.maxLeasePeriod) {
+    const currentDate = new Date();
+    const targetLeaseCommence = new Date(
+      currentDate.getFullYear() - hdbLease + Number(queries.maxLeasePeriod),
+      currentDate.getMonth()
+    );
+
+    queryBuilder.andWhere('resale.leaseCommenceYear <= :maxDate', {
+      maxDate: targetLeaseCommence,
+    });
+  }
 };
 
 const getResalesByIsland = async (
@@ -19,14 +106,8 @@ const getResalesByIsland = async (
     .select('resale.town', 'town')
     .addSelect('CAST(AVG(resale.resalePrice) AS int)', 'resalePrice');
 
-  if (queries.years) {
-    queryBuilder.where(
-      'date_part(\'year\', "transactionDate") IN (:...years)',
-      {
-        years: queries.years,
-      }
-    );
-  }
+  addWhereClauses(queries, queryBuilder);
+
   queryBuilder.groupBy('town');
 
   return queryBuilder.getRawMany();
@@ -57,14 +138,7 @@ const getResalesByTown = async (
     .addSelect('CAST(AVG(resale.resalePrice) AS int)', 'resalePrice')
     .where('coordinates IS NOT NULL');
 
-  if (queries.years) {
-    queryBuilder.andWhere(
-      'date_part(\'year\', "transactionDate") IN (:...years)',
-      {
-        years: queries.years,
-      }
-    );
-  }
+  addWhereClauses(queries, queryBuilder);
 
   if (queries.town) {
     queryBuilder.andWhere({ town: queries.town });
