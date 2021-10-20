0c8b0866d75f2c1094111db57a55ae4159dc7fd5
Seow Alex <seowalex@hotmail.com>
2021-10-20 20:18:41 +0800

    Allow sorting of table

--- frontend/src/components/heatmap/map/FlatMarker.tsx
+++ frontend/src/components/heatmap/map/FlatMarker.tsx
@@ -9,6 +9,7 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  TableSortLabel,
 } from '@mui/material';
 import { Marker } from '@react-google-maps/api';
 import { Close as CloseIcon } from '@mui/icons-material';
@@ -24,11 +25,43 @@ interface Props {
   transactions: FlatTransaction[];
 }
 
+type Order = 'asc' | 'desc';
+type OrderBy = 'flatType' | 'resalePrice' | 'transactionMonth';
+
 const markerOptions: google.maps.MarkerOptions = {
   opacity: 0,
 };
 
+const descendingComparator = <T extends unknown>(
+  a: T,
+  b: T,
+  orderBy: keyof T
+) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const getComparator = <Key extends keyof FlatTransaction>(
+  order: Order,
+  orderBy: Key
+): ((
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number) =>
+  order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+
 const FlatMarker = ({ address, coordinates, transactions }: Props) => {
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<OrderBy>('transactionMonth');
   const [marker, setMarker] = useState<google.maps.Marker>();
   const [showTransactions, setShowTransactions] = useState(false);
 
@@ -42,6 +75,12 @@ const FlatMarker = ({ address, coordinates, transactions }: Props) => {
     marker?.setOptions({
       opacity: 0,
     });
+  };
+
+  const handleSort = (property: OrderBy) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
   };
 
   return (
@@ -78,34 +117,70 @@ const FlatMarker = ({ address, coordinates, transactions }: Props) => {
           <Table size="small" stickyHeader>
             <TableHead>
               <TableRow>
-                <TableCell>Flat Type</TableCell>
-                <TableCell align="right">Resale Price</TableCell>
-                <TableCell align="right">Resale Registration Date</TableCell>
+                <TableCell
+                  sortDirection={orderBy === 'flatType' ? order : false}
+                >
+                  <TableSortLabel
+                    active={orderBy === 'flatType'}
+                    direction={orderBy === 'flatType' ? order : 'asc'}
+                    onClick={() => handleSort('flatType')}
+                  >
+                    Flat Type
+                  </TableSortLabel>
+                </TableCell>
+                <TableCell
+                  sortDirection={orderBy === 'resalePrice' ? order : false}
+                  align="right"
+                >
+                  <TableSortLabel
+                    active={orderBy === 'resalePrice'}
+                    direction={orderBy === 'resalePrice' ? order : 'asc'}
+                    onClick={() => handleSort('resalePrice')}
+                  >
+                    Resale Price
+                  </TableSortLabel>
+                </TableCell>
+                <TableCell
+                  sortDirection={orderBy === 'transactionMonth' ? order : false}
+                  align="right"
+                >
+                  <TableSortLabel
+                    active={orderBy === 'transactionMonth'}
+                    direction={orderBy === 'transactionMonth' ? order : 'asc'}
+                    onClick={() => handleSort('transactionMonth')}
+                  >
+                    Resale Registration Date
+                  </TableSortLabel>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {transactions.map((transaction) => (
-                <TableRow
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell>
-                    {convertFlatTypeToFrontend(transaction.flatType)}
-                  </TableCell>
-                  <TableCell align="right">
-                    {currencyFormatter.format(transaction.resalePrice)}
-                  </TableCell>
-                  <TableCell align="right">
-                    {format(
-                      parse(
-                        transaction.transactionMonth,
-                        'yyyy-MM',
-                        new Date()
-                      ),
-                      'MMM yyyy'
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))}
+              {transactions
+                .slice()
+                .sort(getComparator(order, orderBy))
+                .map((transaction) => (
+                  <TableRow
+                    key={JSON.stringify(transaction)}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell>
+                      {convertFlatTypeToFrontend(transaction.flatType)}
+                    </TableCell>
+                    <TableCell align="right">
+                      {currencyFormatter.format(transaction.resalePrice)}
+                    </TableCell>
+                    <TableCell align="right">
+                      {format(
+                        parse(
+                          transaction.transactionMonth,
+                          'yyyy-MM',
+                          new Date()
+                        ),
+                        'MMM yyyy'
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
             </TableBody>
           </Table>
         </DialogContent>
