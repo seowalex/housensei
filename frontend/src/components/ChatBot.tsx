a660d2d0b528f7b4e2f93850b4e32e075997bb0e
Seow Alex <seowalex@hotmail.com>
2021-10-27 23:20:26 +0800

    Pop up chat bot message on first load

--- frontend/src/components/ChatBot.tsx
+++ frontend/src/components/ChatBot.tsx
@@ -1,28 +1,13 @@
 import { ChatWidget } from '@papercups-io/chat-widget';
 
-const ChatBot = () => {
-  const GREETING =
-    'Hello! Thank you for visiting Housensei! How do you find Housensei so far? Any feedback is greatly appreciated :)';
-  const TITLE = 'Welcome to Housensei';
-  const SUBTITILE = 'Feel free to provide any feedback you have 😊';
-
-  return (
-    <ChatWidget
-      token={import.meta.env.VITE_CHAT_TOKEN}
-      inbox="e1b231f1-e569-49ae-ae10-de77578a5d7c"
-      title={TITLE}
-      subtitle={SUBTITILE}
-      primaryColor="#1890ff"
-      greeting={GREETING}
-      newMessagePlaceholder="Start typing..."
-      showAgentAvailability={false}
-      agentAvailableText="We're online right now!"
-      agentUnavailableText="We're away at the moment."
-      requireEmailUpfront={false}
-      iconVariant="outlined"
-      baseUrl="https://app.papercups.io"
-    />
-  );
-};
+const ChatBot = () => (
+  <ChatWidget
+    token={import.meta.env.VITE_CHAT_TOKEN}
+    title="Welcome to Housensei!"
+    subtitle="Feel free to provide any feedback you have 😊"
+    greeting="Hello! Thank you for visiting Housensei! How do you find Housensei so far? Any feedback is greatly appreciated :)"
+    popUpInitialMessage
+  />
+);
 
 export default ChatBot;
