334a26336a8b7648c692aa7e861a3de910d0b1a2
Seow Alex <seowalex@hotmail.com>
2021-10-27 21:27:05 +0800

    Add compression

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
   css: {
     modules: {
       localsConvention: 'dashesOnly',
